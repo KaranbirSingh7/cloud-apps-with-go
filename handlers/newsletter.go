8c471b53cde63743021daed87f6e71575c8e87d6
karanbirsingh7 <karanbirsingh1161@gmail.com>
2022-10-15 11:07:15 -0400

    feat: add newsletter signup logic

--- handlers/newsletter.go
+++ handlers/newsletter.go
@@ -6,32 +6,30 @@ import (
 
 	"canvas/model"
 	"canvas/views"
+
+	"github.com/gorilla/mux"
 )
 
 type signupper interface {
 	SignupForNewsletter(ctx context.Context, email model.Email) (string, error)
 }
 
-type signupperMock struct{}
+func NewsletterSignupWrapper(m *mux.Router, s signupper) {
+	m.HandleFunc("/newsletter/signup", func(w http.ResponseWriter, r *http.Request) {
+		email := model.Email(r.FormValue("email"))
 
-func (s signupperMock) SignupForNewsletter(ctx context.Context, email model.Email) (string, error) {
-	return "", nil
-}
+		if !email.IsValid() {
+			http.Error(w, "email is invalid", http.StatusBadRequest)
+			return
+		}
 
-func NewsletterSignup(w http.ResponseWriter, r *http.Request) {
-	email := model.Email(r.FormValue("email"))
+		if _, err := s.SignupForNewsletter(r.Context(), email); err != nil {
+			http.Error(w, "error signing up, refresh to try again", http.StatusBadGateway)
+			return
+		}
+		http.Redirect(w, r, "/newsletter/thanks", http.StatusFound)
 
-	var s signupperMock
-	if !email.IsValid() {
-		http.Error(w, "email is invalid", http.StatusBadRequest)
-		return
-	}
-
-	if _, err := s.SignupForNewsletter(r.Context(), email); err != nil {
-		http.Error(w, "error signing up, refresh to try again", http.StatusBadGateway)
-		return
-	}
-	http.Redirect(w, r, "/newsletter/thanks", http.StatusFound)
+	})
 }
 
 func NewsletterThanks(w http.ResponseWriter, r *http.Request) {
--- server/routes.go
+++ server/routes.go
@@ -2,11 +2,21 @@ package server
 
 import (
 	"canvas/handlers"
+	"canvas/model"
+	"context"
 )
 
 func (s *Server) setupRoutes() {
 	s.mux.HandleFunc("/", handlers.IndexPage)
 	s.mux.HandleFunc("/healthz", handlers.Healthz)
-	s.mux.HandleFunc("/newsletter/signup", handlers.NewsletterSignup)
+	// s.mux.HandleFunc("/newsletter/signup", handlers.NewsletterSignup)
+	handlers.NewsletterSignupWrapper(s.mux, &signupperMock{}) //handle: /newsletter/signup URL
 	s.mux.HandleFunc("/newsletter/thanks", handlers.NewsletterThanks)
+
+}
+
+type signupperMock struct{}
+
+func (s signupperMock) SignupForNewsletter(ctx context.Context, email model.Email) (string, error) {
+	return "", nil
 }
