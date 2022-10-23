066bef3c503ca591757ab7431d35294d81148879
karanbirsingh7 <karanbirsingh1161@gmail.com>
2022-10-23 13:12:59 -0400

    test: add integration test for servicebus

--- integrationtest/queue.go
+++ integrationtest/queue.go
@@ -1,1 +1,21 @@
 package integrationtest
+
+import (
+	"canvas/infra"
+	"canvas/messaging"
+
+	"github.com/maragudk/env"
+)
+
+func CreateQueue() (*messaging.Queue, func()) {
+	env.MustLoad("../.env-test")
+
+	queueOpts, err, cleanup := infra.CreateAzureServiceBusResource(env.GetStringOrDefault("AZURE_SUBSCRIPTION_ID", "xxx"), env.GetStringOrDefault("AZURE_RESOURCE_GROUP_NAME", "myTestRGWooho"))
+	if err != nil {
+		panic(err)
+	}
+
+	queue := messaging.NewQueue(*queueOpts)
+
+	return queue, cleanup
+}
