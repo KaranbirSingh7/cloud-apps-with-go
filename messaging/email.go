465c434b7960934df3d18fd62c35b919d26cd713
karanbirsingh7 <karanbirsingh1161@gmail.com>
2022-10-29 11:36:07 -0400

    feat: add email newsletter feature

--- messaging/email.go
+++ messaging/email.go
@@ -1,11 +1,15 @@
 package messaging
 
 import (
+	"bytes"
 	"canvas/model"
 	"context"
 	"embed"
+	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
+	"strings"
 	"time"
 
 	"go.uber.org/zap"
@@ -57,11 +61,91 @@ func createNameAndEmail(name, email string) nameAndEmail {
 	return fmt.Sprintf("%v <%v>", name, email)
 }
 
-func (e *Emailer) SendNewsletterConfirmationEmail(ctx context.Context, to model.Email, token string) {
+func (e *Emailer) SendNewsletterConfirmationEmail(ctx context.Context, to model.Email, token string) error {
 	keywords := map[string]string{
-		"base_url": e.baseURL,
-		"action_url": e.baseURL + "/newsletter/confirm?token=" + token
+		"base_url":   e.baseURL,
+		"action_url": e.baseURL + "/newsletter/confirm?token=" + token,
 	}
 
-	return e.sed
+	// this will return error/nil if anything goes wrong with send email
+	return e.send(
+		ctx,
+		requestBody{
+			MessageStream: transactionMessageStream,
+			From:          e.transactionalFrom,
+			To:            to.String(),
+			Subject:       "AYO confirm your subscription to canvas newsletter",
+			HtmlBody:      getEmail("confirmation_email.html", keywords),
+			TextBody:      getEmail("confirmation_email.txt", keywords),
+		},
+	)
+}
+
+// requestBody used in Emailer.send
+// See https://postmarkapp.com/developer/user-guide/send-email-with-api
+
+type requestBody struct {
+	MessageStream string
+	From          nameAndEmail
+	To            nameAndEmail
+	Subject       string
+	HtmlBody      string
+	TextBody      string
+}
+
+// send using postmark API
+func (e *Emailer) send(ctx context.Context, body requestBody) error {
+	bodyAsBytes, err := json.Marshal(body)
+	if err != nil {
+		return fmt.Errorf("error marshaling request body to json: %w", err)
+	}
+
+	// create NEW request
+	request, err := http.NewRequestWithContext(ctx, http.MethodPost, "https://api.postmarkapp.com/email",
+		bytes.NewReader(bodyAsBytes))
+	if err != nil {
+		return fmt.Errorf("error creating request: %w", err)
+	}
+	// set headers
+	request.Header.Set("Accept", "application/json")
+	request.Header.Set("Content-Type", "application/json")
+	request.Header.Set("X-Postmark-Server-Token", e.token)
+
+	// make POST call with data
+	response, err := e.client.Do(request)
+	if err != nil {
+		return fmt.Errorf("error marking request: %w", err)
+	}
+	defer response.Body.Close()
+
+	// read response
+	bodyAsBytes, err = io.ReadAll(response.Body)
+	if err != nil {
+		return fmt.Errorf("error reading response body: %w", err)
+	}
+
+	// check statusCode
+	if response.StatusCode > 299 {
+		e.log.Info("Error sending email",
+			zap.Int("status", response.StatusCode), zap.String("response", string(bodyAsBytes)))
+		return fmt.Errorf("error sending email, got status: %d", response.StatusCode)
+	}
+
+	// everything good, return no error
+	return nil
+}
+
+// getEmail from given path, panic on errors (because this is important to bundle with codebase)
+func getEmail(path string, keywords map[string]string) string {
+	email, err := emails.ReadFile("emails/" + path)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	emailString := string(email)
+	for keyword, replacement := range keywords {
+		emailString = strings.ReplaceAll(emailString, "{{"+keyword+"}}", replacement)
+	}
+
+	return emailString
 }
